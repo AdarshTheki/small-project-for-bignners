65d58960a02dde321fe80aa9da84725916010738
Adarsh Verma <adarshverma549@gmail.com>
2023-06-10 20:03:37 +0530

    Files - sever.js

---
+++ react-contact-form/src/Server/sever.js
@@ -0,0 +1,29 @@
+// https://w3collective.com/react-contact-form/
+// Create a new file called server.js and first load the required dependencies:
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// Next we use express() to setup the server that’ll run on port 5000:
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "***************@gmail.com",
+    pass: "********",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
