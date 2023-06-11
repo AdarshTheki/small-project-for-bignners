b1b7a18d19406b06b9e0b21103d0d0b111064f90
Adarsh Verma <adarshverma549@gmail.com>
2023-06-11 08:27:59 +0530

    file - App.js

--- react-contact-form/src/App.js
+++ react-contact-form/src/App.js
@@ -1,4 +1,7 @@
-import SignInForms from './Components_1/SignInForms'
+import SignInForms from './Components/SignInForms'
+// import MyFirebase from './Components/FormWithFirebase'
+// import MyForm from './Components/ContactForm'
+// import Form from './Components/NormalForms'
 
 function App() {
   return (
